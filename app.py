375ce06650823d97b4b9b8901218eaaf45ae176b
Filemon279 <280202@uwr.edu.pl>
2017-06-12 18:09:57 +0200

    koment

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#####################################
+#	IMPORT BIBLIOTEK				#
+#####################################
 import os
 from datetime import datetime
 from flask import Flask, render_template, jsonify, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 import time
 
+#####################################
+#		KONFIGURACJA				#
+#####################################
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.sqlite3'
 app.config['SECRET_KEY'] = "random string"
@@ -16,6 +22,9 @@ ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']
 
 db = SQLAlchemy(app)
 
+#####################################
+#	AKTUALIZACJA REKORDU			#
+#####################################
 def updateRecord(id_number,db):
    projectToUpdate = projects.query.filter_by(id=id_number).first()
    projectToUpdate.name = request.form['name']
@@ -27,17 +36,26 @@ def updateRecord(id_number,db):
    projectToUpdate.editDate = time.strftime("%H:%M:%S")
    db.session.commit()
 
+#####################################
+#	DODAWANIE ZDJĘCIA				#
+#####################################
 def addPhoto(id_number,db,photo_URL):
    projectToUpdate = projects.query.filter_by(id=id_number).first()
    if (projectToUpdate.pictures==""): projectToUpdate.pictures += photo_URL
    else: projectToUpdate.pictures += ","+photo_URL
    db.session.commit()
 
+#####################################
+#	USUWANIE REKORDU				#
+#####################################
 def deleteRecord(id_number,db):
    projectToUpdate = projects.query.filter_by(id=id_number).first()
    projectToUpdate.deleteDate = time.strftime("%H:%M:%S")
    db.session.commit()
 
+#####################################
+#	MODEL PROJEKTU DO BAZY DANYCH	#
+#####################################
 class projects(db.Model):
    id = db.Column('project_id', db.Integer, primary_key = True)
    name = db.Column(db.String(100))
@@ -63,11 +81,17 @@ class projects(db.Model):
       self.editDate =  time.strftime("%H:%M:%S");     #Wiem, Data, na razie jest godzina
       self.deleteDate = ""                            #Trzeba to przemyśleć.
 
+######################################
+#TEMPLATE - INDEX PRZEKAZUJE WSZYSTKO#
+######################################
 @app.route('/')
 def show_all():
    return render_template('show_all.html', projects = projects.query.all() )
 
 
+#####################################
+#	PRZEKAZYWANIE PLIKU DO FOLDERU 	#
+#####################################
 @app.route('/upload/<int:id_number>', methods=['POST'])
 def upload(id_number):
     if request.method == 'POST':
@@ -81,9 +105,15 @@ def upload(id_number):
             addPhoto(id_number,db,filename)
             return jsonify({"success":True})
 
+#####################################
+#	NAZWA ORAZ ROZSZERZENIE			#
+#####################################
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+#####################################
+#	TEMPLATE DODAWANIA REKORDU		#
+#####################################
 @app.route('/new', methods = ['GET', 'POST'])
 def new():
    if request.method == 'POST':
@@ -99,8 +129,9 @@ def new():
    return render_template('new.html')
 
 
-
-
+#####################################
+#	DODAWANIE ZDJECIA TEMPLATE		#
+#####################################
 @app.route('/uploadPhotos/<int:id_number>', methods = ['GET', 'POST'])
 def upload_Photos(id_number):
    if request.method == 'POST':
@@ -113,6 +144,9 @@ def upload_Photos(id_number):
    return render_template('uploadPhotos.html', project = projects.query.filter_by(id=id_number).first() )
 
 
+#####################################
+#	EDYTOWANIE WPISU				#
+#####################################
 @app.route('/edit/<int:id_number>', methods = ['GET', 'POST'])
 def edit_project(id_number):
    if request.method == 'POST':
@@ -128,6 +162,9 @@ def edit_project(id_number):
    print(pics_array)
    return render_template('edit.html', project =projectObject , pictures=pics_array )
 
+#####################################
+#	USUWANIE PROJEKTU				#
+#####################################
 @app.route('/delete_project/<int:id_number>', methods = ['GET', 'POST'])
 def delete_project(id_number):
    if request.method == 'POST': 
@@ -136,6 +173,9 @@ def delete_project(id_number):
       return redirect(url_for('show_all'))
    return render_template('delete_project.html', project = projects.query.filter_by(id=id_number).first()  )
 
+#####################################
+#	USUWANIE ZDJECIA Z BAZY			#
+#####################################
 @app.route('/delete_pic/<int:id_number>/<picture>', methods = ['GET','POST'])
 def delete_pic(id_number,picture):
    projectToUpdate = projects.query.filter_by(id=id_number).first()
@@ -148,19 +188,21 @@ def delete_pic(id_number,picture):
 
 
 
-
+#####################################
+#	IMPORT BIBLIOTEK				#
+#####################################
 if __name__ == '__main__':
    db.create_all()
    app.run(debug = True)
 
-
+#####################################
+#	WEZ JEDEN PROJEKT				#
+#####################################
 def getProject(id_number):
    return projects.query.filter_by(id=id_number).first()
 
-
+#####################################
+#	WEZ PROJEKTY OD INDEXU + LICZBA	#
+#####################################
 def getProjects(index,amount):
    return projects.query.filter_by(id>=index and id<(index+abount))
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
